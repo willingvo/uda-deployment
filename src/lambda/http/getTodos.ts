779629dfe33d1a48f67f8fe458d4946d622b012d
NguyenVo <NguyenVo@ge.com>
2022-08-07 17:32:55 +0700

    Init

---
+++ src/lambda/http/getTodos.ts
@@ -0,0 +1,24 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+// import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
+// import { getAllTodos } from '../../businessLogicLayer/todos'
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  createLogger(`Processing get todos with event: ${event}`)
+  // const userId = getUserId(event)
+
+  // const toDos = await getAllTodos(userId)
+  const toDos = {};
+
+  return {
+    statusCode: 200,
+    //added for cors handling
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({items: toDos})
+  }
+}
